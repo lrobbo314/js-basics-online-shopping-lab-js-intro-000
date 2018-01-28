b2b0722ab33d431d70dbf324712c9545f61911bc
Robert Lego <robertlego@gmail.com>
2018-01-28 07:25:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function viewCart() {
 
   let itemsAndPrices = [];
 
-  for ( let i =0; i < l; i++) {
+  for (let i = 0; i < l; i++) {
     let itemAndPrice = cart[i];
     let item = Object.keys(itemAndPrice)[0];
     let price = itemAndPrice[item];
@@ -36,19 +36,19 @@ function viewCart() {
     itemsAndPrices.push(`${item} at \$${price}`);
   }
 
-  switch(itemsAndPrices.length) {
+  switch (itemsAndPrices.length) {
     case 1:
       break;
     case 2:
       itemsAndPrices = itemsAndPrices.join(" and");
       break
     case 3:
-      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices[l - 1] = "and ".concat(itemsAndPrices[l - 1]);
       itemsAndPrices = itemsAndPrices.join(", ");
   }
 
   console.log(`In your cart you have ${itemsAndPrices}.`)
-  
+
 }
 
 function total() {
