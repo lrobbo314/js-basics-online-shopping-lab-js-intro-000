9b07da092b4603781164013480aed6a4ff8662e4
Robert Lego <robertlego@gmail.com>
2018-01-28 07:12:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,3 +89,6 @@ function placeOrder(cardNumber) {
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   cart = [];
 }
+g(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  cart = [];
+}
